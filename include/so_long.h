b3e9d7b646d9ac5b514cb7f1efe03d8e882d0786
Alexis Bonnard <abonnard@student.42nice.fr>
2025-01-05 18:42:36 +0100

    exit texture crashes

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: abonnard <abonnard@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 15:02:36 by abonnard          #+#    #+#             */
-/*   Updated: 2025/01/05 18:01:01 by abonnard         ###   ########.fr       */
+/*   Updated: 2025/01/05 18:26:18 by abonnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,5 +105,6 @@ void	initialize_game(t_map *map, char *file);
 int		init_textures(t_map *map);
 int		check_file_extension(char *file);
 void	update_exit_texture(t_map *map);
+void	put_exit_texture(t_map *map, int x, int y);
 
 #endif
